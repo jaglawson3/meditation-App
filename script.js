a75ea9248382eb49bfe206b3a92db5f2d6782e30
Justin Lawson <jaglawson3@gmail.com>
2015-11-17 15:03:35 -0700

    changes

--- script.js
+++ script.js
@@ -1,109 +1,64 @@
-$(function(){
-  $.get('zen-quote_koan-library.json', function(data){
+$(function() {
+  $.get('zen-quote_koan-library.json', function(data) {
     koans = data.koans;
     quotes = data.quotes;
     console.log(data);
 
   })
 
-
-
-  $("input.set").click(function() {
-      var duration = $('.durationControl').val()
-      alert( "Your duration has been set.");
-      localStorage.setItem("duration", duration);
- });
-
- // $("input.submit").click(function() {
- //     var displayed = $('.focusPoint').val()
- //     sessionStorage.setItem("displays", displayed);
- //
- // });
-
-
-  randomKoan = function(data){
-      var $zenDisplayKoanTitle = $(".zenDisplayKoanTitle");
-      var $zenDisplayKoanText = $(".zenDisplayKoanText");
-      var $zenDisplayKoanSource = $(".zenDisplayKoanSource");
-      var $zenDisplayQuoteText = $(".zenDisplayQuoteText");
-      var $zenDisplayQuoteAuthor = $(".zenDisplayQuoteAuthor");
-      var randomKoan = koans[Math.floor(Math.random()*koans.length)];
-      $zenDisplayKoanTitle.empty();
-      $zenDisplayKoanText.empty();
-      $zenDisplayKoanSource.empty();
-      $zenDisplayQuoteText.empty();
-      $zenDisplayQuoteAuthor.empty();
-      $zenDisplayKoanTitle.append(randomKoan.title);
-      $zenDisplayKoanText.append(randomKoan.text);
-      $zenDisplayKoanSource.append(randomKoan.source);
+  randomKoan = function(data) {
+    var $zenDisplayKoanTitle = $(".zenDisplayKoanTitle");
+    var $zenDisplayKoanText = $(".zenDisplayKoanText");
+    var $zenDisplayKoanSource = $(".zenDisplayKoanSource");
+    var $zenDisplayQuoteText = $(".zenDisplayQuoteText");
+    var $zenDisplayQuoteAuthor = $(".zenDisplayQuoteAuthor");
+    var randomKoan = koans[Math.floor(Math.random() * koans.length)];
+    $zenDisplayKoanTitle.empty();
+    $zenDisplayKoanText.empty();
+    $zenDisplayKoanSource.empty();
+    $zenDisplayQuoteText.empty();
+    $zenDisplayQuoteAuthor.empty();
+    $zenDisplayKoanTitle.append(randomKoan.title);
+    $zenDisplayKoanText.append(randomKoan.text);
+    $zenDisplayKoanSource.append(randomKoan.source);
   };
 
-  randomQuote = function(data){
-      var $zenDisplayKoanTitle = $(".zenDisplayKoanTitle");
-      var $zenDisplayKoanText = $(".zenDisplayKoanText");
-      var $zenDisplayKoanSource = $(".zenDisplayKoanSource");
-      var $zenDisplayQuoteText = $(".zenDisplayQuoteText");
-      var $zenDisplayQuoteAuthor = $(".zenDisplayQuoteAuthor");
-      var randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
-      $zenDisplayKoanTitle.empty();
-      $zenDisplayKoanText.empty();
-      $zenDisplayKoanSource.empty();
-      $zenDisplayQuoteText.empty();
-      $zenDisplayQuoteAuthor.empty();
-      $zenDisplayQuoteText.append(randomQuote.text);
-      $zenDisplayQuoteAuthor.append(randomQuote.author)
+  randomQuote = function(data) {
+    var $zenDisplayKoanTitle = $(".zenDisplayKoanTitle");
+    var $zenDisplayKoanText = $(".zenDisplayKoanText");
+    var $zenDisplayKoanSource = $(".zenDisplayKoanSource");
+    var $zenDisplayQuoteText = $(".zenDisplayQuoteText");
+    var $zenDisplayQuoteAuthor = $(".zenDisplayQuoteAuthor");
+    var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    $zenDisplayKoanTitle.empty();
+    $zenDisplayKoanText.empty();
+    $zenDisplayKoanSource.empty();
+    $zenDisplayQuoteText.empty();
+    $zenDisplayQuoteAuthor.empty();
+    $zenDisplayQuoteText.append(randomQuote.text);
+    $zenDisplayQuoteAuthor.append(randomQuote.author)
   };
 
-  nothing = function(){
-      var $zenDisplay = $(".zenDisplay");
-      $zenDisplay.empty();
+  nothing = function() {
+    var $zenDisplay = $(".zenDisplay");
+    $zenDisplay.empty();
   }
 
-  setDisplay = function(selectValue){
+  setDisplay = function(selectValue) {
 
-  if(selectValue == "koan"){
+    if (selectValue == "koan") {
       randomKoan();
-    }
-    else if(selectValue == "quote"){
+    } else if (selectValue == "quote") {
       randomQuote();
-    }
-    else if(selectValue == "nothing"){
+    } else if (selectValue == "nothing") {
       nothing();
     }
   };
 
-$("input.submit").click(function(event){
-  event.preventDefault();
-  $focusPoint = $(".focusPoint select")
-  setDisplay($focusPoint.val())
-  console.log($focusPoint.val())
+  $("input.submit").click(function(event) {
+    event.preventDefault();
+    $focusPoint = $(".focusPoint select")
+    setDisplay($focusPoint.val())
+    console.log($focusPoint.val())
+  });
 });
-
-  // $("input.submit").click(function) {
-  //   //this is where the code will go to start the timer
-  // });
-
-//     var $jumbotron = $('#jumbotron');
-//
-//     appendRandomKoan = function(data){
-//     for (i=0;i<koans.length; i++){
-//         console.log(koans[i].source);
-//           $jumbotron.append(koans[Math.floor(Math.random()*koans.length)]);
-//       }
-//     }
-//
-//     appendRandomQuote = function(data){
-//       for (i=0;i<quotes.length; i++){
-//           console.log(quotes[i].author);
-//             $jumbotron.append(quotes[Math.floor(Math.random()*quotes.length)]);
-//       }
-//     }
-//
-//     generateRandomQuote = function(){
-//       $jumbotron.append(quotes[Math.floor(Math.random()*quotes.length)]);
-//     }
-//
-//
-// });
-
-})
--- timer.js
+++ timer.js
@@ -1,72 +1,50 @@
-// the date when the countdown has to end yyyy, mm(starting from 0 not from 1), dd, hh, mm, ss
-// for months 0 == january, 1 == february
-start = function(){
+$(function(){
 
-var duration = $('.durationControl').val()
-//
-// if(duration != 0){
-//   countdown = $('#countdown'),
-//   hours = countdown.find('.hours span'),
-//   minutes = countdown.find('.minutes span'),
-//   seconds = countdown.find('.seconds span'),
-//   setTimer;
+  $("input.set").click(function() {
+    event.preventDefault();
+    var duration = $('.durationControl').val();
+    if(duration == 0){
+          alert("Duration can be set in minutes only.")
+    }
+    else {
+          alert( "Your duration has been set.");
+          localStorage.setItem("duration", duration);
+    }
+  });
 
-setTimer = function () {
-//need to figure how to link these logic with input from furationControl
+  function timerDisplay(minutes, seconds){
+      var timer = document.getElementById("timer");
+      var minutesDisplay = minutes + "minutes";
+      var secondsDisplay = seconds + "seconds";
 
-  event.preventDefault();
+      timer.innerText = minutesDisplay + ":" + secondsDisplay;
+  };
 
-  if(duration != 0) {
+  function countDown(){
+      var minutes = parseInt(localStorage.getItem("duration"));
+      console.log(minutes);
+      var seconds = 0;
+      // var timer = getElementByClassName("timer");
+      interval = setInterval(function(){
+          if(seconds == 0 && minutes == 0){
+            clearInterval(interval);
+            return;
+            //this is where we will put the stuff that plays the bell sound
+          }
+          else if (seconds == 0){
+            minutes--;
+            seconds = 59;
+          }
+          else{
+            seconds--;
+          }
+          timerDisplay(minutes, seconds);
+          console.log(seconds);
+      }, 1000)
+    }
 
-  d = Math.floor(time_left / 86400);
-  time_left -= d * 86400;
-
-  h = Math.floor(time_left/3600);
-  time_left -= h*3600;
-
-  m = Math.floor(time_left/60);
-  time_left -= m*60;
-
-  days.html(d);
-  hours.html(h);
-  minutes.html(m);
-  seconds.html(Math.floor(time_left));
-
-
-
-  var timeLeft = ((Days > 0) ? Days + " days " : "") + hours + ":" + seconds)
-
-  console.log(timeLeft)
-  //     Timer.innerHTML = TimeStr;
-
-};
-};
-$("input.start").click(function(event){
-  Timer.innerHTML = TimeStr;
+    $("input.start").click(function(event){
+      event.preventDefault();
+      countDown();
+    });
 });
-
-// function timer() {
-//   var buddhistBell = new Audio("buddhistBell.wav");
-//   if (TotalSeconds <= 0) {
-//     alert("You have completed your meditation")
-//     buddhistBell.play();
-// }
-//
-// TotalSeconds -= 1;
-// UpdateTimer()
-// window.setTimeout("Tick()", 1000);
-// }
-//
-// function UpdateTimer() {
-//     var Minutes = Math.floor(Seconds / 60);
-//     Seconds -= Minutes * (60);
-//     var TimeStr = ((Days > 0) ? Days + " days " : "") + LeadingZero(Minutes) + ":" + LeadingZero(Seconds)
-//     Timer.innerHTML = TimeStr;
-// }
-//
-// function LeadingZero(Time) {
-//     return (Time < 10) ? "0" + Time : + Time;
-// }
-//
-// var buddhistBell = new Audio("buddhistBell.wav"); // buffers automatically when created
-// buddhistBell.play();
