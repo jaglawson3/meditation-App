3fdba6e510dbef7d0d4f3e7c03f34b7ee7c0a949
Justin Lawson <jaglawson3@gmail.com>
2015-11-16 21:30:27 -0700

    added to koan library, also altered zenDisplay divs to better fit text within circle zen shape div

--- script.js
+++ script.js
@@ -21,9 +21,6 @@ $(function(){
  //
  // });
 
-
-
-
   randomKoan = function(data){
       var $zenDisplayKoanTitle = $(".zenDisplayKoanTitle");
       var $zenDisplayKoanText = $(".zenDisplayKoanText");
